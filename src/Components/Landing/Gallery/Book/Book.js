61fa170480f7e853e562d169b7192eadedd20285
tiffchuasx <tiffchuasx@gmail.com>
2017-07-10 11:59:46 +0800

    book component added change button

--- src/Components/Landing/Gallery/Book/Book.js
+++ src/Components/Landing/Gallery/Book/Book.js
@@ -1,26 +1,38 @@
 //Importing required packages
 import React, { Component } from 'react';
-
 //Importing static assets (i.e. stylesheets, images)
 import './Book.css';
-
 console.log("Start of Component Book.js.");
-
 class Book extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+          isReserved: false,
+        }
+    }
+  changeColor(){
+            this.setState({isReserved: !this.state.isReserved})
+    }
+  onMouseEnterHandler = (e) => {
+    console.log(e.target.id)
   }
-
-  render() {
-    return (
-      <div>
-        <h2>Component - Book {this.props.title}</h2>
-      </div>
-    );
-  }
+        render(){
+        let bgColor = this.state.isReserved ? "red" : "green"
+        return (
+          <div className="col-md-3">
+            <button style={{backgroundColor: bgColor}}
+                    onClick={this.changeColor.bind(this)}>Reserve</button>
+            <div className="bookpic"><h1>BOOKPIC</h1></div>
+            <div className="bookreview"
+                  id="bookreview here"
+                onHover={this.onMouseEnterHandler}>hello moto</div>
+            <div className="bookinfo">
+              Title:
+              Author:
+            </div>
+            <div className="reservebook"></div>
+          </div>
+      )
+    }
 }
-
-
-console.log("End of Component Book.js.");
-
 export default Book;
