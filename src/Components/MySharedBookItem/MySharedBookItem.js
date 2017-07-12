cb36727988f99b8261bf8527a8fbba84121bda20
Woody See <woodyseesk@gmail.com>
2017-07-13 02:05:55 +0800

    Added MySharedBookItem style corrections for book thumbnail

--- src/Components/MySharedBookItem/MySharedBookItem.js
+++ src/Components/MySharedBookItem/MySharedBookItem.js
@@ -18,30 +18,30 @@ class MySharedBookItem extends Component {
     let bgColor = "rgba(0,103,109,0.7)";
 
     return (
-      <div className="row sharedbookitem-container">
+      <div className="row booklistitem-container">
         <div className="row">
-          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 sharedbookitem-left">
-            <span className="sharedbookitem-thumbnail">
+          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 booklistitem-left">
+            <span className="booklistitem-thumbnail">
               <img src={this.props.contents.cover} alt="..." />
             </span>
           </div>
-          <div className="col-lg-8 col-md-8 col-sm-8 col-xs-12 sharedbookitem-info">
+          <div className="col-lg-8 col-md-8 col-sm-8 col-xs-12 booklistitem-info">
             <h3>{this.props.contents.title}</h3>
             <h4>{this.props.contents.author}</h4>
             <h5>{this.props.contents.genre}</h5>
-            <div className="sharedbookitemreview"
+            <div className="booklistitemreview"
                  id={this.props.contents.title}
                  onMouseOver={this.onMouseOver}>
               <h5><b>Owner:</b> @{this.props.contents.owner.username}</h5>
             </div>
-            <hr className="sharedbookitem-footer-line"/>
+            <hr className="booklistitem-footer-line"/>
           </div>
         </div>
         <div className="row">
           <div className="col-lg-10 col-md-10 col-sm-12 col-xs-12">
           </div>
           <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12">
-            <button className="btn btn-success sharedbookitem-update-btn"
+            <button className="btn btn-success booklistitem-update-btn"
                     style={{backgroundColor: bgColor}}
                     onClick={this.onClick}
                     id="test">
